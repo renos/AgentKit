89d8c20232deda6ba1c6799d5b34be984dc43dac
Renos Zabounidis <rzabounidis@protonmail.com>
2025-02-10 13:16:27 -0800

    bug fix

--- src/agentkit/llm_api/GPT.py
+++ src/agentkit/llm_api/GPT.py
@@ -8,6 +8,7 @@ import os
 from .utils import match_model
 from .base import BaseModel
 
+
 def initialize_client():
     if os.environ.get("OPENAI_KEY"):
         return OpenAI(
@@ -21,11 +22,13 @@ def initialize_client():
             azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
         ), os.environ.get("AZURE_DEPLOYMENT_NAME")
 
-    openai_key_path = os.path.join(os.path.expanduser('~'), ".openai", "openai.key")
-    azure_key_path = os.path.join(os.path.expanduser('~'), ".openai", "azure_openai.key")
+    openai_key_path = os.path.join(os.path.expanduser("~"), ".openai", "openai.key")
+    azure_key_path = os.path.join(
+        os.path.expanduser("~"), ".openai", "azure_openai.key"
+    )
 
     if os.path.exists(openai_key_path):
-        with open(openai_key_path, 'r') as f:
+        with open(openai_key_path, "r") as f:
             lines = f.read().splitlines()
             if len(lines) >= 2:
                 return OpenAI(
@@ -38,16 +41,19 @@ def initialize_client():
             The first line should be your API key and the second line should be your organization ID.
             """
         )
-    
+
     if os.path.exists(azure_key_path):
-        with open(azure_key_path, 'r') as f:
+        with open(azure_key_path, "r") as f:
             lines = f.read().splitlines()
             if len(lines) >= 3:
-                return AzureOpenAI(
-                    api_key=lines[0].strip(),
-                    api_version=lines[1].strip(),
-                    azure_endpoint=lines[2].strip(),
-                ), lines[3].strip()
+                return (
+                    AzureOpenAI(
+                        api_key=lines[0].strip(),
+                        api_version=lines[1].strip(),
+                        azure_endpoint=lines[2].strip(),
+                    ),
+                    lines[3].strip(),
+                )
 
         raise FileNotFoundError(
             """
@@ -58,9 +64,17 @@ def initialize_client():
             """
         )
 
+
 client_and_deployment = initialize_client()
-client = client_and_deployment[0] if isinstance(client_and_deployment, tuple) else client_and_deployment
-deployment_name = client_and_deployment[1] if isinstance(client_and_deployment, tuple) else None
+client = (
+    client_and_deployment[0]
+    if isinstance(client_and_deployment, tuple)
+    else client_and_deployment
+)
+deployment_name = (
+    client_and_deployment[1] if isinstance(client_and_deployment, tuple) else None
+)
+
 
 class GPT_chat(BaseModel):
 
@@ -69,29 +83,45 @@ class GPT_chat(BaseModel):
         self.enc_fn = enc_fn
         self.model_max = model_max
         super().__init__(model_name, global_counter, "chat")
-    
+
     def encode(self, txt):
         return self.enc_fn.encode(txt)
 
     def decode(self, txt):
         return self.enc_fn.decode(txt)
 
-    def query_chat(self, messages, shrink_idx, max_gen=512, temp=0.):
+    def query_chat(self, messages, shrink_idx, max_gen=512, temp=0.0):
         model_max = self.model_max
-        messages = self.shrink_msg(messages, shrink_idx, model_max-max_gen)
-        while(True):
+        messages = self.shrink_msg(messages, shrink_idx, model_max - max_gen)
+        while True:
             try:
-                completion = client.chat.completions.create(
-                    model=deployment_name if deployment_name else self.name,
-                    messages=messages,
-                    temperature=temp,
-                    max_tokens=max_gen,
-                )
-                return completion.choices[0].message.content, {"prompt":completion.usage.prompt_tokens, "completion":completion.usage.completion_tokens, "total":completion.usage.total_tokens}
+                if self.name == "o1-mini-2024-09-12":
+                    completion = client.chat.completions.create(
+                        model=deployment_name if deployment_name else self.name,
+                        messages=messages,
+                    )
+                else:
+                    completion = client.chat.completions.create(
+                        model=deployment_name if deployment_name else self.name,
+                        messages=messages,
+                        temperature=temp,
+                        max_tokens=max_gen,
+                    )
+                return completion.choices[0].message.content, {
+                    "prompt": completion.usage.prompt_tokens,
+                    "completion": completion.usage.completion_tokens,
+                    "total": completion.usage.total_tokens,
+                }
             except Exception as e:
                 if self.debug:
                     raise e
-                elif isinstance(e, openai.RateLimitError) or isinstance(e, openai.APIStatusError) or isinstance(e, openai.APITimeoutError) or isinstance(e, openai.APIConnectionError) or isinstance(e, openai.InternalServerError):
+                elif (
+                    isinstance(e, openai.RateLimitError)
+                    or isinstance(e, openai.APIStatusError)
+                    or isinstance(e, openai.APITimeoutError)
+                    or isinstance(e, openai.APIConnectionError)
+                    or isinstance(e, openai.InternalServerError)
+                ):
                     time.sleep(30)
                     print(e)
                 elif "However, your messages resulted in" in str(e):
@@ -99,10 +129,19 @@ class GPT_chat(BaseModel):
                     e = str(e)
                     index = e.find("your messages resulted in ")
                     import re
-                    val = int(re.findall(r'\d+', e[index + len("your messages resulted in ") : ])[0])
+
+                    val = int(
+                        re.findall(
+                            r"\d+", e[index + len("your messages resulted in ") :]
+                        )[0]
+                    )
                     index2 = e.find("maximum context length is ")
-                    model_max = int(re.findall(r'\d+', e[index2 + len("maximum context length is "):])[0])
-                    messages = self.shrink_msg_by(messages, shrink_idx, val-model_max)
+                    model_max = int(
+                        re.findall(
+                            r"\d+", e[index2 + len("maximum context length is ") :]
+                        )[0]
+                    )
+                    messages = self.shrink_msg_by(messages, shrink_idx, val - model_max)
                 else:
                     print("error:", e)
                     print("retrying in 5 seconds")
--- src/agentkit/llm_api/utils.py
+++ src/agentkit/llm_api/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 try:
     import tiktoken
 except ImportError:
@@ -7,39 +8,50 @@ import difflib
 
 
 enc_fns = {
-    "gpt-4":tiktoken.encoding_for_model("gpt-4"),
-    "gpt-4o":tiktoken.encoding_for_model("gpt-4"),
-    "gpt-4-turbo":tiktoken.encoding_for_model("gpt-4"),
-    "gpt-4-1106-preview":tiktoken.encoding_for_model("gpt-4"),
-    "gpt-4-32k-0613":tiktoken.encoding_for_model("gpt-4"),
-    "gpt-3.5-turbo":tiktoken.encoding_for_model("gpt-3.5-turbo"),
-    "gpt-3.5-turbo-1106":tiktoken.encoding_for_model("gpt-3.5-turbo"),
-    "gpt-3.5-turbo-0125":tiktoken.encoding_for_model("gpt-3.5-turbo"),
+    "gpt-4": tiktoken.encoding_for_model("gpt-4"),
+    "gpt-4o": tiktoken.encoding_for_model("gpt-4"),
+    "o1-mini-2024-09-12": tiktoken.encoding_for_model("gpt-4"),
+    "gpt-4-turbo": tiktoken.encoding_for_model("gpt-4"),
+    "gpt-4-1106-preview": tiktoken.encoding_for_model("gpt-4"),
+    "gpt-4-32k-0613": tiktoken.encoding_for_model("gpt-4"),
+    "gpt-3.5-turbo": tiktoken.encoding_for_model("gpt-3.5-turbo"),
+    "gpt-3.5-turbo-1106": tiktoken.encoding_for_model("gpt-3.5-turbo"),
+    "gpt-3.5-turbo-0125": tiktoken.encoding_for_model("gpt-3.5-turbo"),
     "claude-3": None,
     "claude-2.1": None,
     "ollama": None,
+    "sambanova": None,
+    "google": None,
 }
 model_maxes = {
-    "gpt-4":8192,
-    "gpt-4o":128000,
-    "gpt-4-turbo":128000,
-    "gpt-4-1106-preview":128000,
-    "gpt-4-32k-0613":32768,
-    "gpt-3.5-turbo":16385,
-    "gpt-3.5-turbo-1106":16385,
-    "gpt-3.5-turbo-0125":16385,
-    "claude-3":50000,
-    "claude-2.1":50000,
-    "ollama":8192,
+    "gpt-4": 8192,
+    "o1-mini-2024-09-12": 128000,
+    "gpt-4o": 128000,
+    "gpt-4-turbo": 128000,
+    "gpt-4-1106-preview": 128000,
+    "gpt-4-32k-0613": 32768,
+    "gpt-3.5-turbo": 16385,
+    "gpt-3.5-turbo-1106": 16385,
+    "gpt-3.5-turbo-0125": 16385,
+    "claude-3": 50000,
+    "claude-2.1": 50000,
+    "ollama": 8192,
+    "sambanova": 8192,
+    "google": 16385,
 }
+
 
 def match_model(model_name):
 
     # match model name to the closest string in enc_fns.keys()
     model_name = model_name.lower()
-    if model_name.startswith('ollama'):
-        model_name = 'ollama'
-    
+    if model_name.startswith("ollama"):
+        model_name = "ollama"
+    elif model_name.startswith("sambanova"):
+        model_name = "sambanova"
+    elif model_name.startswith("google"):
+        model_name = "google"
+
     matches = []
     matches += [model for model in model_maxes.keys() if model_name.startswith(model)]
     # sort by length of match
@@ -50,7 +62,11 @@ def match_model(model_name):
         raise ValueError("Model name {} not found!".format(model_name))
     else:
         model = matches[0]
-    print("Matched model_name {} to: {}. If this match is not accurate, then the token counter will not function properly.".format(model_name, model))
+    print(
+        "Matched model_name {} to: {}. If this match is not accurate, then the token counter will not function properly.".format(
+            model_name, model
+        )
+    )
 
     model_max = model_maxes[model]
     enc_fn = enc_fns[model]
